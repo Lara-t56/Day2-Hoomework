0d53befd74a80758dc1d999527489e24d445112b
Lara-t56 <129784890+Lara-t56@users.noreply.github.com>
2023-04-05 14:49:19 +0300

    Add files via upload

---
+++ Day2- Main.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Q1-------------------------------------
+      Scanner s =new Scanner(System.in);
+
+//        System.out.println("Enter your number:");
+//        int number =s.nextInt();
+//        if (number>0){
+//            System.out.println("The number is positive");
+//        } else if (number<0) {
+//            System.out.println("The number is negative");
+//        }else {
+//            System.out.println("The number is 0");
+//        }
+
+        //Q2-------------------------------------------
+
+//        System.out.println("Enter your name:");
+//        String n = s.nextInt();
+//        System.out.println("Enter your number:");
+//        int num = s.nextInt();
+//        System.out.println(n.charAt(num));
+//        System.out.println(n.contains(n));
+
+        //Q3 ------------------------------------------
+
+//        System.out.println("Enter your name:");
+//        String name=s.nextInt();
+//        System.out.println("Enter your letter");
+//        char cha=s.next().charAt(0);
+//        if (name.contains(""+cha+"")){
+//            System.out.println("your letter"+ ""+cha+""+ " is:");
+//        }else{
+//            System.out.println("is your letter"+ ""+cha+""+"is not found");
+//        }
+//
+        //Q4--------------------------------------------------
+
+//        System.out.println("Enter the first number:");
+//        int num1 =s.nextInt();
+//        System.out.println("Enter the second number:");
+//        int num2 =s.nextInt();
+//        System.out.println("Enter the third number:");
+//        int num3 =s.nextInt();
+//        if (num1>num2 && num1>num3 ){
+//            System.out.println("first number"+"("+num1+")"+"the largest number");
+//        } else if (num2>num1 && num2>num3) {
+//            System.out.println("second number"+"("+num2+")"+"the largest number");
+//        }else{
+//            System.out.println("third number"+"("+num3+")"+ "the largest number");
+//        }
+
+    }
+}
